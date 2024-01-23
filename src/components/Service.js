0a56f1706894cd71d485b7f9ed74a18c356f7230
NavMapp <95779113+NavanMapp@users.noreply.github.com>
2024-01-23 17:25:53 +0200

    Services section

---
+++ src/components/Service.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function Service() {
+  return (
+    <div className='sevices'>
+        <h1>See what services we offer</h1>
+        <div>
+            <div className='service_card'>
+                <h2>Hardware Support</h2>
+                <p>Laptops, Desktops & Printers</p>
+                <button>Log Repair</button>
+            </div>
+              <div className='service_card'>
+                  <h2>Software Support</h2>
+                  <p>Installation & Maintenance</p>
+                  <button>Read More</button>
+              </div>
+              <div className='service_card'>
+                  <h2>Network Support</h2>
+                  <p>Switches, Routers & Cabling</p>
+                  <button>Read More</button>
+              </div>
+              <div className='service_card'>
+                  <h2>Server Support</h2>
+                  <p>Configuring & Administration</p>
+                  <button>Read More</button>
+              </div>
+        </div>
+    </div>
+  )
+}
+
+export default Service
