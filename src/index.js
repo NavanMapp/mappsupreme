99501f7ed684c2214e1021317cb757d615e1b7f5
NavMapp <95779113+NavanMapp@users.noreply.github.com>
2024-01-28 18:58:31 +0200

    create pages & links

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { BrowserRouter, Routes, Route } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Contact from './pages/Contact';
+import NoPage from './pages/NoPage';
+
+export default function Index() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />} />
+        <Route index element={<Home />} />
+        <Route path='login' element={<Login />} />
+        <Route path='Contact' element={<Contact />} />
+        <Route path='*' element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/pages/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Outlet, Link } from 'react-router-dom'
+
+function Layout() {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default Layout
