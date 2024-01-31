323ddda59b0b86da31d4eb7b3201b741b7a81317
Návan <navanmapp@gmail.com>
2024-01-31 11:23:13 +0200

    signup/login update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import  { React } from 'react';
 import { createBrowserRouter,
-          BrowserRouter,
-          Routes, Route,
           RouterProvider } from 'react-router-dom'
 import ReactDOM from 'react-dom/client';
 import './index.css';
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,26 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../images/logo.png'
+import '../styles/login.css'
+import google from '../images/google-logo.png'
+import microsoft from '../images/microsoft-logo.png'
 
 function Signup() {
     return (
-        <div>
-            <a href='/'>
-                <img src={logo} alt="MAPP SUPREME" className="App-logo" />
-            </a>
-            <h1>Welcome back</h1>
-            <h2>Get started !!!😁</h2>
-            <form>
-                <input type='text' name='Email Address' placeholder='Email address' />
-                <input type='password' name='password' placeholder='Password' />
-                <button className='button' >Continue</button>
+        <div className='login-container'>
+            <form className='login-form'>
+                <div className='login-heading'>
+                    <a href='/'>
+                        <Link to='/'>
+                            <img src={logo} alt="MAPP SUPREME" className="logo" />
+                        </Link>
+                    </a>
+                    <h1>Create an account</h1>
+                </div>
+                <input type='text' name='Email Address'
+                    placeholder='Email address' className='login-input' />
+                <input type='password' name='password'
+                    placeholder='Password' className='login-input' />
+                <button className='login-submit' >Continue</button>
             </form>
             <label>Already have an account?</label>
-            <a href='/auth/login'><Link to='/auth/login' className=''>  Login</Link></a>
-            <ul>OR</ul>
-            <form>
-                <button className=''>Continue with Google</button>
-                <button className=''>Continue with Microsoft Account</button>
+            <a href='/auth/login'><Link to='/auth/login' className='login-link'>  Login</Link></a>
+            <ul className='login-heading'> OR </ul>
+            <form className='btn-container'>
+                <button className='google-btn'><img src={google} alt='Google Account' />Continue with Google</button>
+                <button className='microsoft-btn'><img src={microsoft} alt='Google Account' />Continue with Microsoft Account</button>
             </form>
         </div>
     )
