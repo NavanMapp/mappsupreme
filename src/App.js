0c6a40a096f34af664f37521e7bc0b826d8dce2f
Návan <navanmapp@gmail.com>
2024-02-21 09:19:47 +0200

    Final updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Link } from 'react'
+import React from 'react'
 import Banner from './components/Banner';
 import About from './components/About';
 import Service from './components/Service';
